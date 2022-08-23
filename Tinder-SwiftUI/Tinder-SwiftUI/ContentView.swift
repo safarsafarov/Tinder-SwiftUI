418f016eeedd2d21e422793ba4b37fe3675a4d89
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-23 15:41:31 +0500

    ✨

---
+++ Tinder-SwiftUI/Tinder-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tinder-SwiftUI
+//
+//  Created by Safar Safarov on 23/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tinder-SwiftUI/Tinder-SwiftUI/Tinder_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinder_SwiftUIApp.swift
+//  Tinder-SwiftUI
+//
+//  Created by Safar Safarov on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Tinder_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
