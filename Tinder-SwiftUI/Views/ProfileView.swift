04fe6468540232a20500d347dd467c5df5ef8d50
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-24 14:06:33 +0500

    ✨

--- Tinder-SwiftUI/Views/ProfileView.swift
+++ Tinder-SwiftUI/Views/ProfileView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Image("img_jd")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 120, height: 120)
+                .cornerRadius(60)
+                .shadow(radius: 5)
+            VStack(spacing: 5) {
+                Text("JD")
+                    .font(.system(size: 30))
+                    .fontWeight(.heavy)
+                Text("iOS Engineer")
+                Text("Uzbekistan, Tashkent")
+            }
+            ZStack {
+                HStack(spacing: 50) {
+                    VStack(spacing: 8) {
+                        Image(systemName: "gearshape.fill")
+                            .font(.system(size: 35, weight: .heavy))
+                    }
+                }
+            }
+        }
     }
 }
 
